c5a526a297d2e0468a2226b7c74e291108007bcc
Nathan L. Walls <nathan@rexluther.com>
2010-11-28 21:13:28 -0500

    Initial addition from the content project
    - Using ActiveModel in place of ActiveRecord

---
+++ lib/validates_url_safety.rb
@@ -0,0 +1,26 @@
+ActiveModel::Base.class_eval do
+  def self.converts_whitespace_for_url(*attr_names)
+    configuration = { :if => :allow_validation }
+    configuration.update(attr_names.extract_options!)
+
+    replace = /(\ |\_)/
+
+    validates_each(attr_names) do |record, attr_name, value|
+      unless record.send( attr_name ).nil?
+        record.send( attr_name).gsub!(replace, '-')
+      end
+    end
+  end
+
+  def self.validates_as_url_safe(*attr_names)
+    char_class = /[^0-9A-Za-z\_\-]/
+
+    validates_each(attr_names) do |record, attr_name, value|
+      unless record.send( attr_name ).nil?
+        if record.send(attr_name).match char_class
+          record.errors.add(attr_name, "contains illegal characters")
+        end
+      end
+    end
+  end
+end
